23cb6ca1317495d74464d254c20c6d7a2946a407
anmvk58 <an.cnttk58@gmail.com>
2025-03-04 04:15:34 +0700

    fix token in code

--- shipper/utils.py
+++ shipper/utils.py
@@ -1,11 +1,12 @@
 import requests
+from decouple import config
 
-URL = "https://api-man1.kiotviet.vn/api/invoices/list"
+URL = config('API')
 
 HEADERS = {
         "accept": "application/json, text/plain, */*",
         "accept-language": "vi,en-US;q=0.9,en;q=0.8",
-        "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IndYYSJ9.eyJpc3MiOiJrdnNzand0Iiwic3ViIjoxMDAxNDg0ODgwLCJpYXQiOjE3Mzk5ODE4NzYsImV4cCI6MTc0MjQwMTA3NiwicHJlZmVycmVkX3VzZXJuYW1lIjoiMDU4ODU4MjcxNSIsInJvbGVzIjpbIlVzZXIiXSwia3Zzb3VyY2UiOiJSZXRhaWwiLCJrdnVzZXRmYSI6MCwia3Z3YWl0b3RwIjowLCJrdnNlcyI6IjllNDczM2UwZDM3NjQ1N2E4ZGFjNTY0YzVkOGQwY2VlIiwia3Z1aWQiOjEwMDE0ODQ4ODAsImt2bGFuZyI6InZpLVZOIiwia3Z1dHlwZSI6MCwia3Z1bGltaXQiOiJGYWxzZSIsImt2dWFkbWluIjoiVHJ1ZSIsImt2dWFjdCI6IlRydWUiLCJrdnVsaW1pdHRyYW5zIjoiRmFsc2UiLCJrdnVzaG93c3VtIjoiVHJ1ZSIsImt2YmkiOiJGYWxzZSIsImt2YmlkIjoxMDAwMDE4MzQyLCJrdnJpbmRpZCI6OSwia3ZyY29kZSI6IjA1ODg1ODI3MTUiLCJrdnJpZCI6NTAwNjg5NzMxLCJrdnVyaWQiOjUwMDY4OTczMSwia3ZyZ2lkIjozNiwicGVybXMiOiIifQ.pHtVjCz0egRM9DfAcnfa8ei6q_OA9GWp_YUzincyCu2Nr_SuHCqgHZ3fnrMvIQbKBr2A-5uBOzESukVmd7rm25Ce1XF6d3UX4ud-iFEw7RUlMqr02HhYXSV1IyZ-W0ORcRB0Dra7x_2WdNxVrJizYqPt2fH24CraA7NDIHNvFpjZjorDgb3Iyn7fZD3BzDleXVEFz27Htl1DNtc51XCRGEmA8nUtdWOFsA4TpI_TWiiOA66JduhNnNbnILRnw7p5Jj1GR9BiEz5QuCkYMG0blnm92lRvta70QAkkGFaM8GCXvtnXEFQ4rRDt1ludxYJhWGmth6Qbmg24aQlRgF0OnQ",
+        "authorization": "Bearer " + config('TOKEN'),
         "branchid": "1000018342",
         "content-type": "application/json; charset=utf-8",
         "fingerprintkey": "211d1f5bb8cc08a94863d2291f1c866d_Chrome_Desktop_Máy tính Windows",
